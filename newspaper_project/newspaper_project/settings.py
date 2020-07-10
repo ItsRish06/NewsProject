63d76596f72ef3e61439e376df090ea3c61d370d
ItsRish06 <66073563+ItsRish06@users.noreply.github.com>
2020-07-10 21:12:20 +0530

    1

--- newspaper_project/newspaper_project/settings.py
+++ newspaper_project/newspaper_project/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
+    #3rd party
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,4 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL='users.CustomUser'
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='home'
+CRISPY_TEMPLATE_PACK='bootstrap4'
