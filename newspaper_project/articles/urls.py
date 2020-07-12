c52761e4ac197cd87385617ede835655bd4e2f8d
ItsRish06 <66073563+ItsRish06@users.noreply.github.com>
2020-07-12 21:24:33 +0530

    authentication & authorization

--- newspaper_project/articles/urls.py
+++ newspaper_project/articles/urls.py
@@ -9,12 +9,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('<int:pk>/edit/',
-         ArticleUpdateView.as_view(), name='article_edit'),
-    path('<int:pk>/',
-         ArticleDetailView.as_view(), name='article_detail'),
-    path('<int:pk>/delete/',
-         ArticleDeleteView.as_view(), name='article_delete'),
+    path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
     path('new/', ArticleCreateView.as_view(), name='article_new'),
     path('', ArticleListView.as_view(), name='article_list'),
 ]
